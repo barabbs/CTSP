3a1a6fbca6e45c6e0dfaca0821424537904e6da2
Alessandro Sosso <alesosso@gmail.com>
2024-06-07 02:44:19 +0200

    added cache for combinatorial generation of instances

--- modules/combinatorics.py
+++ modules/combinatorics.py
@@ -1,9 +1,11 @@
 from itertools import permutations, combinations
 from modules.coding import Coding, Cover
+from functools import cache
 
 import logging
 
 
+@cache
 def partition(n, maximum=float("inf"), minimum=2):
     if n <= maximum:
         yield (n,)
@@ -11,7 +13,7 @@ def partition(n, maximum=float("inf"), minimum=2):
         for tail in partition(n - i, i):
             yield (i, *tail)
 
-
+@cache
 def cycles(nodes, parts, minimum=None):
     if len(parts) == 0:
         yield tuple()
@@ -36,5 +38,16 @@ def graph_codings_generator(n, k=2):
     for i_p, p_1 in enumerate(partitions):
         c_1 = tuple(nodes[sum(p_1[:i]):sum(p_1[:i + 1])] for i, p in enumerate(p_1))
         for p_2 in partitions[i_p:]:
-            for c_2 in cycles(set(nodes), p_2):
+            for c_2 in cycles(frozenset(nodes), p_2):
                 yield Coding((c_1, c_2))
+
+
+"""
+4 3.2901763916015625e-05
+5 4.3392181396484375e-05
+6 0.0004787445068359375
+7 0.0025649070739746094
+8 0.06202578544616699
+9 0.4634256362915039
+10 4.7264580726623535
+11 54.881959438323975"""
