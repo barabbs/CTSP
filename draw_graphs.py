b0df350da09c76813f13d34a064b73296ce55be7
Alessandro Sosso <alesosso@gmail.com>
2024-06-25 23:55:08 +0200

    bugfix and improvement

--- draw_graphs.py
+++ draw_graphs.py
@@ -2,8 +2,8 @@ import os
 from modules import var
 from modules.models import get_models, GRAPH
 from modules.ctsp import initialize_database
-from modules.graph import Graph
-from main import STRATEGIES_TABLE
+from main import STRATEGIES_TABLE, GENERATORS_TABLE
+from modules.calculations import Calculators, CANON, CERTIFICATE, SUBT_EXTR, GAP, CALCULATIONS
 
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -21,22 +21,30 @@ parser.add_argument("-w", "--weights", type=int, default=None,
                     help="weights of covers in graph (sequence separated by whitespace)\n\n")
 parser.add_argument("-b", "--best", type=int, default=None,
                     help="number of graphs with highest gap to draw\n\n")
-parser.add_argument("-g", "--gap", action="store_true",
+parser.add_argument("-g", "--only_gap", action="store_true",
                     help="only draw graphs with an integrality gap (implicit when -b specified)\n\n")
 parser.add_argument("-s", "--strategy", type=str, default=var.DEFAULT_STRATEGY,
                     help=f"selected strategy for computation, from the following (default: {var.DEFAULT_STRATEGY})" + STRATEGIES_TABLE)
+parser.add_argument("--generator", type=str, default=var.DEFAULT_GENERATOR,
+                    help=f"selected generator for computation, from the following (default: {var.DEFAULT_GENERATOR})" + GENERATORS_TABLE)
+for calc_type in (CANON, CERTIFICATE, SUBT_EXTR, GAP):
+    CALC_ROWS = '\n'.join(f"{i:>4}. {c.CALC_NAME}" for i, c in enumerate(CALCULATIONS[calc_type]))
+    parser.add_argument(f"--{calc_type}", type=int, default=0,
+                        help=f"selected calculation for {calc_type.upper()}, among the following (default: 0)\n" + CALC_ROWS + "\n\n")
 
 
-def run(n, k=2, weights=None, strategy=var.DEFAULT_STRATEGY, only_gap=True, n_best=None):
+def run(n, k=2, weights=None, strategy=var.DEFAULT_STRATEGY, only_gap=True, n_best=None,
+        generator=var.DEFAULT_GENERATOR, calculators=None):
     manager = enlighten.get_manager()
     weights = weights or (1,) * k
     metadata, models = get_models(n, k, weights)
     engine = initialize_database(metadata=metadata, models=models,
-                                 n=n, k=k, weights=weights, strategy=strategy)
+                                 n=n, k=k, weights=weights,
+                                 strategy=strategy, generator=generator, calculators=calculators)
     with Session(engine) as session:
         statement = select(models[GRAPH])
         if only_gap:
-            statement = statement.where(models[GRAPH].gap.is_not(None))
+            statement = statement.where(models[GRAPH].gap.is_not(None)).order_by(models[GRAPH].gap.desc())
         graphs = session.scalars(statement).all()
         progbar = manager.counter(total=n_best or len(graphs), desc=f"Drawing", leave=False)
         for i, graph in enumerate(graphs):
@@ -50,8 +58,18 @@ def run(n, k=2, weights=None, strategy=var.DEFAULT_STRATEGY, only_gap=True, n_be
 
 if __name__ == '__main__':
     args = parser.parse_args()
+    gap = args.only_gap or args.best is not None
+    calcs_indices = dict((calc_type, getattr(args, calc_type, 0)) for calc_type in (CANON, CERTIFICATE, SUBT_EXTR, GAP))
+    calculators = Calculators(calcs_indices)
     for k in args.k:
         if args.weights is not None:
             assert sum(args.weights) == k
         for n in args.n:
-            run(n=n, k=k, weights=args.weights, strategy=args.strategy.upper(), only_gap=args.gap, n_best=args.best)
+            if n > 8 and not gap:
+                ans = input(
+                    f"n = {n} is very high to draw all graphs (use -b or -g).\nDo you want to proceed anyway? (y/n)  > ")
+                if ans.upper() != "Y":
+                    continue
+            run(n=n, k=k, weights=args.weights,
+                strategy=args.strategy.upper(), generator=args.generator.lower(), calculators=calculators,
+                only_gap=gap, n_best=args.best)
