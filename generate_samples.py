c3c4980d4251400d245626174476fade1b03b49c
Alessandro Sosso <alesosso@gmail.com>
2024-09-25 17:12:35 +0200

    added no_gap

--- generate_samples.py
+++ generate_samples.py
@@ -12,7 +12,7 @@ import enlighten
 
 
 def run(n_range, k=2, weights=None, strategy=var.DEFAULT_STRATEGY, generator=var.DEFAULT_GENERATOR, calcs_indices=None,
-        samples=100):
+        samples=100, no_gap=False):
     manager = enlighten.get_manager()
     weights = weights or (1,) * k
     for n in n_range:
@@ -35,8 +35,11 @@ def run(n_range, k=2, weights=None, strategy=var.DEFAULT_STRATEGY, generator=var
                 with open(os.path.join(dirpath, f"sample_n{n:02}_{i:03}.txt"), 'w') as f:
                     gr = g._graph
                     print(gr)
-                    res = gap_calc.calc(gr)
-                    gap = res[var.GRAPH_TABLE]['gap']
+                    if not no_gap:
+                        res = gap_calc.calc(gr)
+                        gap = res[var.GRAPH_TABLE]['gap']
+                    else:
+                        gap = ""
                     print(f"\t{gap}")
                     f.write(f"{gr}\n\nn:   {n}\ngap: {gap}\n\n" +
                             "\n".join(f"{u},{v},{w}" for u, v, w in gr.edge_count_generator(weight=True)))
@@ -47,6 +50,7 @@ def run(n_range, k=2, weights=None, strategy=var.DEFAULT_STRATEGY, generator=var
 
 
 parser.add_argument("--samples", type=int, default=10000)
+parser.add_argument("--no_gap", action="store_true")
 
 if __name__ == '__main__':
     init_logging(level=logging.INFO)
@@ -56,4 +60,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     run(n_range=args.n, k=args.k[0], weights=args.weights,
         strategy=args.strategy.upper(), generator=args.generator.lower(),
-        calcs_indices=calcs_indices)
+        calcs_indices=calcs_indices, no_gap=args.no_gap)
